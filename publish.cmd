2b7bbd3176691bf61fb6c1386a5d2980c0163dde
lkw0820 <90160639+lkw0820@users.noreply.github.com>
2022-05-10 22:45:19 +0900

    Delete publish.cmd

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=greatwallsample-Web-Deploy.pubxml -p:Password=t3QvfKnQJC6Z0AFwvl6cr6cS7FjaSp109cR2vmtmNwHxp99qz7ZonQuldJkl
-
